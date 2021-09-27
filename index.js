48414458c01dc803b3f0ceef962951e8f6126973
Elijah Silverman <elijahmsilverman@gmail.com>
2021-09-27 09:55:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messages = []
+
+function writeCards(names, events) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+  return(messages)
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+      console.log(integer)
+      integer --
+    }
+  }
+  
